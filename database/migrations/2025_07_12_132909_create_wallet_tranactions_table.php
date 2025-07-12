fd0408e915e23647ca8cec7fa548369a6b5ad8fe
kyeyunedaniel <kyeyunedaniel0@gmail.com>
2025-07-12 17:04:23 +0300

    wallet transactions

--- database/migrations/2025_07_12_132909_create_wallet_tranactions_table.php
+++ database/migrations/2025_07_12_132909_create_wallet_tranactions_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('wallet_tranactions', function (Blueprint $table) {
+        Schema::create('wallet_transactions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('wallet_id')->constrained('wallets')->onDelete('cascade');
             $table->string('transaction_hash')->nullable()->comment('Unique hash for idempotency');

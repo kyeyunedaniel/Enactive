a43ef71cd224a8f5aec82e0698b728c1cee77acb
kyeyunedaniel <kyeyunedaniel0@gmail.com>
2025-09-22 02:03:26 +0300

    ensuring one endpoint has access to the route

--- routes/api.php
+++ routes/api.php
@@ -24,7 +24,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/check_public_url_availability', [FrontEndValidationController::class, 'checkUsername']); //->name('validate.checkUsername'); 
 
-Route::get('/pesapal-ipn/log-information', [FrontEndValidationController::class, 'checkUsername']); //->name('validate.checkUsername') 
+// Route::get('/pesapal-ipn/log-information', [FrontEndValidationController::class, 'checkUsername']); //->name('validate.checkUsername') 
 
 Route::post('/pesapal/initiate', [PesapalController::class, 'initiatePayment']);
 

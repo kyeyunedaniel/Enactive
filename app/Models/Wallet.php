10fd2766f018d4e666ad3cdb4d4e06a0c9c0243d
kyeyunedaniel <kyeyunedaniel0@gmail.com>
2025-09-23 13:38:26 +0300

    assigning a bastable for the wallet

--- app/Models/Wallet.php
+++ app/Models/Wallet.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Wallet extends Model
 {
     use HasFactory;
+    protected $table="wallets"; 
+    
     protected $fillable = [
         'user_id',
         'balance',
